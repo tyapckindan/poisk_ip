3388ebead2b3f4148880e2bc37bb9f8ca2fd047c
Данила Тяпкин <dtapkin6@gmail.com>
2023-03-20 10:57:59 +0300

    Новинка, теперь с маской.

--- poisk ip/Program.cs
+++ poisk ip/Program.cs
@@ -13,9 +13,9 @@ internal class Program
 
         Console.WriteLine(buffer);
 
-        Regex regexIp = new(@"\b(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\b");
+        Regex regexIp = new(@"\b(?:(?:25[0-5]|2[0-4]\d|[01]?\d\d?)\.){3}(?:25[0-5]|2[0-4]\d|[01]?\d\d?)\b");
 
-        Console.WriteLine("Истинные айпишники:");
+        Console.WriteLine("Истинные айпишники: ");
 
         MatchCollection matches = regexIp.Matches(buffer);
         if (matches.Count > 0)
@@ -23,6 +23,23 @@ internal class Program
                 Console.WriteLine(match.Value);
         else
             Console.WriteLine("Совпадений не найдено");
+
+
+        Regex regexMask255 = new(@"(^255\.|\.255\.|\.255$)");
+        Regex regexMask0 = new(@"(^0\.|\.0\.|\.0$)");
+
+        Console.WriteLine("\nМаска: ");
+        List<string> ip = new();
+        for(int i = 0; i < matches.Count; i++) ip.Add(matches[i].Value);
+
+        for(int i = 0; i < ip.Count;i++)
+            if (!regexMask255.IsMatch(ip[i]) | !regexMask0.IsMatch(ip[i]))
+            {
+                ip.RemoveAt(i);
+                i--;
+            }
+
+        foreach (string mask in ip) Console.WriteLine(mask);
 
         fIn.Close();
     }
